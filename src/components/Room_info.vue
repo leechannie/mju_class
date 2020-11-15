ce76cf444edcb619bdb522231f15ec1b942d2365
Sol Choi <habrush3024@gmail.com>
2020-11-15 16:39:07 +0900

    20201115-1

--- src/components/Room_info.vue
+++ src/components/Room_info.vue
@@ -7,33 +7,72 @@
             </h3>
         </b-card-text>
     </b-card>
+    <b-table
+        striped hover :fields = "fields"  
+                      :items = "items"              
+    > 
+    
+    </b-table>
+   
 </div>
 </template>
 
 <script>
+import data from "@/data/room.js"
 import firebase from 'firebase'
 export default {
-    props:['document'],
     name: "Room_info",
     data(){
+        
         const roomno = this.$route.params.roomno;
         //const schedule = this.$route.params.schedule;
         //const sn = await db.collction("room").doc(this.document).roomno
         return {
-            roomno : roomno
-        }
-    },
-    created() {
-        this.read()
-    },
-    methods : {
-        async read(){
-            var db = firebase.firestore();
-            var docRef = db.collection("room").doc(String(this.$route.params.roomno));
-            console.log(docRef.get());
-    }
-    
-    }
+        fields : [
+            {
+                key: 'time',
+                label: '시간'
+            },
+            {
+                key: 'rsv_bool',
+                label : '예약여부'
+            }
+        ],
+        roomno : roomno,
+        items : [
+            {time : '9-10',
+            rsv_bool:'o'},
+            {time : '10-11',
+            rsv_bool:'x'},
+            {time : '11-12',
+            rsv_bool:'o'},
+            {time : '12-13',
+            rsv_bool:'x'},
+            {time : '13-14',
+            rsv_bool:'o'},
+            {time : '14-15',
+            rsv_bool:'x'},
+            {time : '15-16',
+            rsv_bool:'o'},
+            {time : '16-17',
+            rsv_bool:'x'},
+            
+        ]
+        };
+    // },
+    // created() {
+    //     this.read()
+    // },
+    // methods : {
+    //     async read(){
+    //         //console.log(items.rsv_bool,items.time)
+    //         return{
+    //             rsv_bool : this.items.rsv_bool,
+    //             time : this.items.time
+                
+    //         }
+    //     }
+    }    
     }
 
 </script>
